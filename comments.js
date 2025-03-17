c5c2f548d6ef1de4f5b5dc0b9a75cc403c5ad021
Anghela Rodriguez <anghela.rodriguez@vallegrande.edu.pe>
2025-03-16 20:53:12 -0500

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,68 +1,16 @@
-//crear web server
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-const app = express();
-app.use(bodyParser.json());
-//crear servidor
-const server = app.listen(3000, () => {
-    console.log('Server running on port 3000');
-    });
+const http = require('http');
 
-app.post('/api/endpoint', (req, res) => {
-    const { data } = req.body;
-    axios.post('http://localhost:4000/api/endpoint', { data })
-    .then(response => {
-    res.send(response.data);
-    })
-    .catch(error => {
-    res.send
-    (error);
-    }
-    );
-    });
+// Crear un servidor web
 
-app.put('/api/endpoint', (req, res) => {
-    const { data } = req.body;
-    axios.put('http://localhost:4000/api/endpoint', { data })
-    .then(response => {
-    res.send(response.data);
-    })
-    .catch(error => {
-    res.send(error);
-    });
-    });
+const hostname = '127.0.0.1';
+const port = 3000;
 
-app.delete('/api/endpoint', (req, res) => {
-    axios.delete('http://localhost:4000/api/endpoint')
-    .then(response => {
-    res.send(response.data);
-    })
-    .catch(error => {
-    res.send(error);
-    });
-    });
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hola, mundo!\n');
+});
 
-app.get('/api/endpoint', (req, res) => {
-    axios.get('http://localhost:4000/api/endpoint')
-    .then(response => {
-    res.send(response.data);
-    })
-    .catch(error => {
-    res.send(error);
-    });
-    }
-    );
-
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-    });
-
-
-
-
-
-
-
-
-
+server.listen(port, hostname, () => {
+    console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+}); 
